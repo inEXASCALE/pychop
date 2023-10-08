f19f71280a2c26bfbdce625601ec19dfa6842036
the null <42580530+chenxinye@users.noreply.github.com>
2023-10-08 08:41:32 +0200

    Update roundit.py

--- pychop/roundit.py
+++ pychop/roundit.py
@@ -5,21 +5,18 @@ def roundit(x, rmode=1, flip=0, p=0.5, t=24, randfunc=None):
 
     sign = lambda x: np.sign(x) + (x==0)
     
-    if hasattr(x, "__len__"):
-        is_arr = True
-    else:
-        is_arr = False
+    if not hasattr(x, "__len__"):
+        x = np.array(x, ndmin=1)
     
     if randfunc is None:
         randfunc = lambda n: np.random.uniform(0, 1, n)
             
     match rmode:
         case 1:
-            y = np.abs(x);
+            y = np.abs(x)
             u = np.round(y - ((y % 2) == 0.5))
             
-            if is_arr:
-                u[np.nonzero(u == -1)] = 0 # Special case, negative argument to ROUND.
+            u[np.nonzero(u == -1)] = 0 # Special case, negative argument to ROUND.
                 
             y = np.sign(x) * u
             
@@ -33,17 +30,16 @@ def roundit(x, rmode=1, flip=0, p=0.5, t=24, randfunc=None):
             y = ((x >= 0) | (x == -np.inf)) * np.floor(x) + ((x < 0) | (x == np.inf)) * np.ceil(x)
             
         case 5 | 6:
-            y = np.abs(x); 
-            frac = y - np.floor(y);
+            y = np.abs(x)
+            frac = y - np.floor(y)
             k = np.nonzero(frac != 0)[0]
-            # print("k:", k, type(k))
             if len(k) == 0:
                 y = x; 
             else:   
                 # Uniformly distributed random numbers
                 
                 rnd = randfunc(len(k))
-                # print("frac:", frac, type(frac))
+                
                 vals = frac[k]
                 
                 if len(vals.shape) == 2:
@@ -53,10 +49,10 @@ def roundit(x, rmode=1, flip=0, p=0.5, t=24, randfunc=None):
                 
                 if rmode == 5: # Round up or down with probability prop. to distance.
                     j = rnd <= vals
-                else: # Round up or down with equal probability.       
+                elif rmode == 6: # Round up or down with equal probability.       
                     j = rnd <= 0.5
-                
-                y[k[j]] = np.ceil(y[k[j]])
+                    
+                y[k[j==0]] = np.ceil(y[k[j==0]])
                 y[k[j!=0]] = np.floor(y[k[j!=0]])
                 y = sign(x)*y
                 
@@ -67,7 +63,8 @@ def roundit(x, rmode=1, flip=0, p=0.5, t=24, randfunc=None):
         temp = np.random.uniform(low=0, high=1, size=y.shape)
         k = np.nonzero(temp <= p); # Indices of elements to have a bit flipped.
         if len(k)==0:
-            u = np.abs(y(k));
+            u = np.abs(y(k))
+            
             # Random bit flip in significand.
             # b defines which bit (1 to p-1) to flip in each element of y.
             # Using SIZE avoids unwanted implicit expansion.
@@ -76,13 +73,14 @@ def roundit(x, rmode=1, flip=0, p=0.5, t=24, randfunc=None):
             # (including the hidden bit) and emax is the maximum value of the
             # exponent.  
 
-            b = randi(t-1, u.shape[0], u.shape[1]);
+            b = randi(t-1, u.shape[0], u.shape[1])
             b = np.random.uniform(low=1, high=t-1, size=u.shape) # % t is an integer with modulus on [0,15].
             # Flip selected bits.
-            u = np.bitwise_xor(u, np.power(2, b-1));
-            y[k] = sign(y(k))*u; 
+            u = np.bitwise_xor(u, np.power(2, b-1))
+            y[k] = sign(y(k))*u
     
     return y
+    
     
     
    
