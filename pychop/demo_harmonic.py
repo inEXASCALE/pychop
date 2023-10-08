6b650822b724da36ff19c44c445f477a0ffca8e6
the null <42580530+chenxinye@users.noreply.github.com>
2023-10-08 08:40:23 +0200

    Update demo_harmonic.py

--- pychop/demo_harmonic.py
+++ pychop/demo_harmonic.py
@@ -3,34 +3,40 @@ import copy
 
 
 np.random.seed(1)
-print('Format  Round mode      Sum      No. terms\n')
-print('------------------------------------------\n')
+print('---------------------------------------------')
+print('Format  Round mode     Sum      No. terms')
+print('---------------------------------------------')
+format_prec = None
+
 for p in np.arange(0, 5):
-    print(p)
     match p:
         case 0:
             prec = 'custom' 
             t = 5
             emax = 3
             params = customs(t, emax)
+            format_prec = prec + '  '
             
         case 1:
             prec = 'bfloat16'
+            format_prec = prec + ''
             
         case 2:
             prec = 'fp16'
+            format_prec = prec + '    '
             
         case 3:
             prec = 'fp8-e4m3'
+            format_prec = prec + ''
             
         case 4:
             prec = 'fp8-e5m2'
+            format_prec = prec + ''
 
 
     for i in np.arange(1, 7):
-
         rmode = i
-        cp = chop(prec= prec, rmode=rmode, customs=params)
+        cp = chop(prec=prec, rmode=rmode, customs=params, random_state=20)
 
         s = 0
         n = 1
@@ -42,4 +48,5 @@ for p in np.arange(0, 5):
                 break
             n = n + 1
         
-        print('{0}    {1:1.0f}    {2:9.4e}     {3:g}'.format(prec,i, s[0], n))
+        print('{0}    {1:1.0f}      {2:9.4e}      {3:g}'.format(format_prec, i, s[0], n))
+    print('------------------------------------------')
