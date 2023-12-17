4b1f79e1ef18a07ef15c3f0a80f7f6377a6df29f
chenxinye <cclcquant@yahoo.com>
2023-12-17 09:38:12 +0100

    add new files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import logging
 
 PRJECT_NAME = "pychop"
 PACKAGE_NAME = "pychop"
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 SETREQUIRES=["numpy", "torch"]
 MAINTAINER="Xinye Chen"
 EMAIL="xinyechenai@gmail.com"
