8087608e877c1b3a4da5aa4ae3a6e0b4db164ed6
the null <cclcquant@yahoo.com>
2024-01-20 08:54:22 +0100

    Update chop.py

--- pychop/numpy/chop.py
+++ pychop/numpy/chop.py
@@ -176,7 +176,7 @@ class chop(object):
             if self.t > self.maxfraction:
                 raise ValueError('Precision of the custom format must be at most')
                 
-        y = self.chop_wrapper(x[:, :])
+        y = self.chop_wrapper(x)
         return y
         
 
