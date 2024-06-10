1de6a10ea2974803d9f3de458a2f728ae9ebd131
chenxinye <cclcquant@yahoo.com>
2024-06-10 18:35:42 +0200

    fix import issues

--- pychop/__init__.py
+++ pychop/__init__.py
@@ -1,7 +1,10 @@
 from .simulate import simulate
 from .np.chop import customs
-from .fixed_point import to_fixed_point
 from .set_backend import backend
+from .float_params import float_params
+from .fixed_point import to_fixed_point
+from .chop import chop
+from .quant import quant
 
 
 __version__ = '0.2.3'  
