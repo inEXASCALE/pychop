86af3408e6c58a548ce9beb4cdf7f0ebe26db4c7
the null <cclcquant@yahoo.com>
2024-06-13 08:31:03 +0200

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -1,4 +1,5 @@
 import unittest
+import pychop
 from pychop import demo_harmonic
 
 
@@ -8,11 +9,15 @@ class TestClassix(unittest.TestCase):
         checkpoint = 1
             
         try:
+            pychop.backend('numpy') 
             demo_harmonic.main() # test floating point running
-
+            pychop.backend('torch')
+            demo_harmonic.main() # test floating point running
+            pychop.backend('jax')
+            demo_harmonic.main() # test floating point running
         except:
             checkpoint = 0
-
+        
         self.assertEqual(checkpoint, 1)
 
 
