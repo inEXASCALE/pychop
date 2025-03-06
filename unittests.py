fix customs precision

unittests.py
@@ -693,11 +693,11 @@ class TestClassix(unittest.TestCase):
         from pychop import Customs
         pychop.backend('numpy', 1) 
 
-        ct1 = customs(emax=15, t=11) 
+        ct1 = Customs(emax=15, t=11) 
         pyq_f = Chop(customs=ct1, rmode=3) 
         emulated1 = pyq_f(X_np)
         
-        ct2 = customs(exp_bits=5, sig_bits=10) 
+        ct2 = Customs(exp_bits=5, sig_bits=10) 
         pyq_f = Chop(customs=ct2, rmode=3)
         emulated2 = pyq_f(X_np)
         assert np.array_equal(emulated1, emulated2)