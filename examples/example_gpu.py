cc331ecc42208bd5975b68261c81be3849b87122
chenxinye <cclcquant@yahoo.com>
2025-03-12 23:07:08 +0100

    test code

--- examples/example_gpu.py
+++ examples/example_gpu.py
@@ -7,9 +7,10 @@ sys.path.append('../')
 import numpy as np
 import torch
 import pychop
-from numpy import linalg
 from time import time
 from pychop.chop import Chop
+
+from pychop.lightchop import LightChop
 # from pychop.quant import quant
 from time import time
 
@@ -21,26 +22,35 @@ if __name__ == '__main__':
 
     pychop.backend('torch', 1) # print information
 
-    pyq_f = Chop('h')
+    ch = Chop('h')
     st = time()
-    pyq_f(X_th)
+    ch(X_th)
     print("runtime 1:", time() - st)
 
-    pychop.backend('torch')
-    pyq_f = Chop('h')
+
+    ch_light = LightChop(exp_bits=5, sig_bits=10, rmode=1)
+    st = time()
+    ch_light(X_th)
+    print("runtime 2:", time() - st)
+    
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     X_gpu = X_th.to(device)
+    st = time()
+    ch(X_gpu)
+    print(device, "runtime 3:", time() - st)
 
     st = time()
-    pyq_f(X_gpu)
-    print(device, " runtime 2:", time() - st)
+    ch_light(X_gpu)
+    print(device, "runtime 4:", time() - st)
 
     # Output
     # python example_gpu.py
     # Load Troch backend.
-    # runtime 1: 0.9266691207885742
-    # cuda  runtime 2: 0.1649622917175293
+    # runtime 1: 0.9214024543762207
+    # runtime 2: 0.7316868305206299
+    # cuda runtime 3: 0.20627665519714355
+    # cuda runtime 4: 0.0255887508392334
 
     # matlab takes: >> tic; emu_val = chop(X); toc -> Elapsed time is 2.084953 seconds. 
