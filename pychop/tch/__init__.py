fff203c17a324963c9546aca1a7b28d39d23ecc5
chenxinye <cclcquant@yahoo.com>
2026-02-07 09:33:45 +0100

    use Front-end wrapper class

--- pychop/tch/__init__.py
+++ pychop/tch/__init__.py
@@ -1,5 +1,5 @@
 from .integer import *
 from .fixed_point import *
 from .bitchop import Bitchop
-from .float_point import _Chop
-from .lightchop import _LightChop, LightChopSTE
+from .float_point import Chop_
+from .lightchop import LightChop_, LightChopSTE
