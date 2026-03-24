78b4695d04470c47e06c12c45ad52a48c3217240
the-null <xinyechenai@gmail.com>
2026-03-24 08:01:03 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'pychop'
 copyright = '2026, InEXASCALE computing'
 author = 'InEXASCALE computing'
-release = '0.5.1'
+release = '0.5.2'
 
 extensions = [
     'sphinx.ext.autodoc',
