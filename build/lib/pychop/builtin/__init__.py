b644e2f7e41912f574c86d5da2284683f295789f
the-null <xinyechenai@gmail.com>
2026-06-05 17:41:44 +0800

    Delete build/lib/pychop directory

--- build/lib/pychop/builtin/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cpfloat import *
-from .cparray import *
-from .cparray_jax import *
-from .cptensor import *
-from . import linalg
--- build/lib/pychop/np/__init__.py
+++
@@ -1,5 +0,0 @@
-from .integer import *
-from .fixed_point import *
-from .bitchop import Bitchop
-from .float_point import Chop_
-from .lightchop import LightChop_
